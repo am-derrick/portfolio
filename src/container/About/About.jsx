c483c2e017c3f83f9b0d1592e5630e63fc46532a
Derrick Ampire <ampire90@gmail.com>
2023-06-06 22:28:54 +0300

    About section added

---
+++ src/container/About/About.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './About.css';
+
+
+function About() {
+    return (
+        <section className='about' id='about'>
+            <div className='container'>
+                <h2>About Me</h2>
+                <p>
+                    I am a Software Engineer with a preference for backend and cloud development with experience in AWS & GCP, Typescript, Javascript and Python.
+                </p>
+                <p>
+                    I have a strong passion for teaching and mentoring and currently use my expertise as a Technical Mentor.
+                </p>
+            </div>
+        </section>
+    );
+}
+
+export default About;
